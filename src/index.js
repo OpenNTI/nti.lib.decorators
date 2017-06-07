4ba48892f03590a69e06ac2207ff86ed0abad2de
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-06-07 17:18:13 -0500

    Explicit export of core-decorators so we can override

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-export * from 'core-decorators';
+export {
+	applyDecorators,
+	autobind,
+	decorate,
+	deprecate,
+	extendDescriptor,
+	lazyInitialize,
+	memoize,
+	override,
+	profile,
+	suppressWarnings,
+	time
+} from 'core-decorators';
 export * from './common';
