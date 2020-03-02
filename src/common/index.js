bc8adc93d2f7468d2521acf7121433957a97b70a
Jonathan Grimes <jsg2021@gmail.com>
2020-03-02 13:36:10 -0600

    Stop using Stage 1 proposal: `export Foo from ...`

--- src/common/index.js
+++ src/common/index.js
@@ -1,6 +1,6 @@
-export configurable from './configurable';
-export enumerable from './enumerable';
-export mixin from './mixin';
-export nonconfigurable from './non-configurable';
-export nonenumerable from './non-enumerable';
-export readonly from './readonly';
+export { default as configurable } from './configurable';
+export { default as enumerable } from './enumerable';
+export { default as mixin } from './mixin';
+export { default as nonconfigurable } from './non-configurable';
+export { default as nonenumerable } from './non-enumerable';
+export { default as readonly } from './readonly';
