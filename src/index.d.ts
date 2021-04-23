caef348897f76de7c21b259da0d528a12c4d5914
Jonathan Grimes <jsg2021@gmail.com>
2021-04-23 16:13:43 -0500

    tweak typedef

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,20 +1,36 @@
 declare module '@nti/lib-decorators' {
-	function mixin<T, A>(a: A): (x: T) => T & A;
-	function mixin<T, A, B>(a: A, b: B): (x: T) => T & A & B;
-	function mixin<T, A, B, C>(a: A, b: B, c: C): (x: T) => T & A & B & C;
+	function mixin<T, A>(a: A): (x: T) => T & ReturnType<A>;
+	function mixin<T, A, B>(
+		a: A,
+		b: B
+	): (x: T) => T & ReturnType<A> & ReturnType<B>;
+	function mixin<T, A, B, C>(
+		a: A,
+		b: B,
+		c: C
+	): (x: T) => T & ReturnType<A> & ReturnType<B> & ReturnType<C>;
 	function mixin<T, A, B, C, D>(
 		a: A,
 		b: B,
 		c: C,
 		d: D
-	): (x: T) => T & A & B & C & D;
+	): (
+		x: T
+	) => T & ReturnType<A> & ReturnType<B> & ReturnType<C> & ReturnType<D>;
 	function mixin<T, A, B, C, D, E>(
 		a: A,
 		b: B,
 		c: C,
 		d: D,
 		e: E
-	): (x: T) => T & A & B & C & D & E;
+	): (
+		x: T
+	) => T &
+		ReturnType<A> &
+		ReturnType<B> &
+		ReturnType<C> &
+		ReturnType<D> &
+		ReturnType<E>;
 	function mixin<T, A, B, C, D, E, F>(
 		a: A,
 		b: B,
@@ -22,7 +38,15 @@ declare module '@nti/lib-decorators' {
 		d: D,
 		e: E,
 		f: F
-	): (x: T) => T & A & B & C & D & E & F;
+	): (
+		x: T
+	) => T &
+		ReturnType<A> &
+		ReturnType<B> &
+		ReturnType<C> &
+		ReturnType<D> &
+		ReturnType<E> &
+		ReturnType<F>;
 	function mixin<T, A, B, C, D, E, F, G>(
 		a: A,
 		b: B,
@@ -31,7 +55,16 @@ declare module '@nti/lib-decorators' {
 		e: E,
 		f: F,
 		g: G
-	): (x: T) => T & A & B & C & D & E & F & G;
+	): (
+		x: T
+	) => T &
+		ReturnType<A> &
+		ReturnType<B> &
+		ReturnType<C> &
+		ReturnType<D> &
+		ReturnType<E> &
+		ReturnType<F> &
+		ReturnType<G>;
 	function mixin<T, A, B, C, D, E, F, G, H>(
 		a: A,
 		b: B,
@@ -41,5 +74,15 @@ declare module '@nti/lib-decorators' {
 		f: F,
 		g: G,
 		h: H
-	): (x: T) => T & A & B & C & D & E & F & G & H;
+	): (
+		x: T
+	) => T &
+		ReturnType<A> &
+		ReturnType<B> &
+		ReturnType<C> &
+		ReturnType<D> &
+		ReturnType<E> &
+		ReturnType<F> &
+		ReturnType<G> &
+		ReturnType<H>;
 }
